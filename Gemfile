ae4e183cbce58707731f7714a416ab85862c6745
Said Rodrigues <coderflemis@gmail.com>
2024-07-24 19:42:06 -0300

    feat: add active storage

--- Gemfile
+++ Gemfile
@@ -75,3 +75,5 @@ gem "shadcn-ui", "~> 0.0.15"
 gem "tailwind_merge", "~> 0.12.0"
 
 gem "erb-formatter", "~> 0.7.3"
+
+gem "aws-sdk-s3", "~> 1.156"
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails/all"
+require_relative 'boot'
+
+require 'rails/all'
+require 'active_storage/engine'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,7 +17,7 @@ module GolDRoger
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
